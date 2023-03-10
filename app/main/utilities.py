cb7603abffe3599f706c7ea45904d62f914e02cb
avorobyov20 <109670774+avorobyov20@users.noreply.github.com>
2023-03-10 11:34:15 +0800

    fix: удаляем концевые пробелы и переводы строки

--- app/main/utilities.py
+++ app/main/utilities.py
@@ -1,3 +1,4 @@
+import re
 from django.template.loader import render_to_string
 from django.core.signing import Signer
 from django.conf import settings
@@ -22,5 +23,6 @@ def send_activation_notification(user):
         host = "http://localhost:8000"
     context = {"user": user, "host": host, "sign": signer.sign(user.username)}
     subject = render_to_string("email/activation_letter_subject.txt", context)
+    subject = re.sub("^\s+|\n|\r|\s+$", '', subject)
     body_text = render_to_string("email/activation_letter_body.txt", context)
     user.email_user(subject, body_text)
