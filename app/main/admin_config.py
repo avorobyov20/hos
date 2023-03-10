99172f483a56e1b7f359c7cae3a560a30b3cdbf9
avorobyov20 <109670774+avorobyov20@users.noreply.github.com>
2023-03-10 22:57:59 +0800

    Update admin_config.py

--- app/main/admin_config.py
+++ app/main/admin_config.py
@@ -15,7 +15,7 @@ from django.urls import path
 
 
 class CustomAdmin(admin.AdminSite):
-    site_header = f"Hearts of Space clone - {settings.HOST_NAME}"
+    site_header = f"{settings.HOST_NAME}"
     index_title = f"Администрирование {settings.SERVICE_NAME}"
 
     def __init__(self, *args, **kwargs):
